c13132ef3b1b45e5c650509f92aadd322c2ba8a5
Henrique B <hb.belgamo@gmail.com>
2024-12-08 14:37:20 -0300

    feat: adds banner and sliders to home page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import {
   AppBar,
   Toolbar,
@@ -13,13 +13,18 @@ import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import LogoutIcon from "@mui/icons-material/Logout";
 import HomeIcon from "@mui/icons-material/Home";
 import ProductCart from "../ProductCart/ProductCart";
+import SearchIcon from "@mui/icons-material/Search";
 
 import { useNavigate } from "react-router-dom";
 
-const NavBar = ({cart, setCart, onSearch, setAuthenticated }) => {
+import products from "../../utils/products.json";
+
+const NavBar = ({ cart, setCart, onSearch, setAuthenticated }) => {
   const navigate = useNavigate();
   const user = JSON.parse(localStorage.getItem("user")) || "Usuário";
+
   const [isCartOpen, setCartOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleLogout = () => {
     localStorage.removeItem("user");
@@ -43,48 +48,72 @@ const NavBar = ({cart, setCart, onSearch, setAuthenticated }) => {
     setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
   };
 
+  const handleSearch = () => {
+    if (!searchTerm.trim()) {
+      navigate("/showcase");
+    } else {
+      const filteredProducts = products.filter((product) =>
+        product.title.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      navigate("/products", {state:{filteredProducts}})
+    }
+  };
+
   return (
     <>
-    <AppBar position="sticky">
-      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <IconButton color="inherit" onClick={() => navigate("/home")}>
-          <HomeIcon></HomeIcon>
-        </IconButton>
+      <AppBar position="sticky">
+        <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+          <IconButton color="inherit" onClick={() => navigate("/home")}>
+            <HomeIcon></HomeIcon>
+          </IconButton>
 
-        <TextField
-          placeholder="Buscar produtos..."
-          variant="outlined"
-          size="small"
-          sx={{
-              backgroundColor: "white",
-              borderRadius: 1,
-              width: "40%",
-            }}
-            onChange={(e) => onSearch(e.target.value)}
+          <Box sx={{ display: "flex", width: "50%" }}>
+            <TextField
+              placeholder="Buscar produtos..."
+              variant="outlined"
+              size="small"
+              sx={{
+                backgroundColor: "white",
+                borderRadius: 1,
+                width: "100%",
+              }}
+              onChange={(e) => setSearchTerm(e.target.value)}
             ></TextField>
+            <IconButton
+              color="secondary"
+              onClick={handleSearch}
+              aria-label="buscar"
+              sx={{
+                backgroundColor: "white",
+                borderRadius: 1,
+              }}
+            >
+              <SearchIcon></SearchIcon>
+            </IconButton>
+          </Box>
 
-        <Box sx={{ display: "flex", alignItems:'center', gap: 2 }}>
-          <Typography variant="body1">Olá, {user.name}</Typography>
+          <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+            <Typography variant="body1">Olá, {user.name}</Typography>
 
-          <IconButton color="inherit" onClick={() => setCartOpen(true)}>
-            <Badge badgeContent={cart.length} color="error">
-              <ShoppingCartIcon></ShoppingCartIcon>
-            </Badge>
-          </IconButton>
+            <IconButton color="inherit" onClick={() => setCartOpen(true)}>
+              <Badge badgeContent={cart.length} color="error">
+                <ShoppingCartIcon></ShoppingCartIcon>
+              </Badge>
+            </IconButton>
 
-          <IconButton color="inherit" onClick={handleLogout}>
-            <LogoutIcon></LogoutIcon>
-          </IconButton>
-        </Box>
-      </Toolbar>
-    </AppBar>
-          <ProductCart
-          onClose={() => setCartOpen(false)}
-          open={isCartOpen}
-          cartItems={cart}
-          onUpdateQuantity={handleUpdateQuantity}
-          onRemoveItem={handleRemoveItem}
-        ></ProductCart>
+            <IconButton color="inherit" onClick={handleLogout}>
+              <LogoutIcon></LogoutIcon>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <ProductCart
+        onClose={() => setCartOpen(false)}
+        open={isCartOpen}
+        cartItems={cart}
+        onUpdateQuantity={handleUpdateQuantity}
+        onRemoveItem={handleRemoveItem}
+      ></ProductCart>
     </>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,111 @@
 import React from "react";
+import { Card, CardMedia, CardContent, Typography, Box } from "@mui/material";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import products from "../utils/products.json";
+import { Navigation, Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 const Home = () => {
-
   return (
     <>
-      <h1>Bem-vindo à Home Page</h1>
-      <p>Você está logado!</p>
+      <Box
+        sx={{
+          width: "100%",
+          height: "300px",
+          backgroundImage:
+            "url(https://res.cloudinary.com/dvqvv2bkq/image/upload/v1733667667/images/photo/mezvvsmaizeg5esekx5c.png)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      ></Box>
+
+      {/* Primeiro Carrosel */}
+      <Typography variant="h5" sx={{ mt: 3, mb: 2, pl: 2 }}>
+        Vouchers
+      </Typography>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        // pagination={{ clickable: true }}
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={4}
+        breakpoints={{
+          320: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 4 },
+        }}
+      >
+        {products.slice(0, 6).map((product) => (
+          <SwiperSlide>
+            <Card
+              sx={{ minWidth: 300, position: "relative", margin: "0 auto" }}
+            >
+              <Box sx={{ position: "relative" }}>
+                <CardMedia
+                  component="img"
+                  height="100%"
+                  image={product.image}
+                  alt={product.title}
+                ></CardMedia>
+                <CardContent>
+                  <Typography variant="h6">{product.title}</Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {" "}
+                    {product.price} pontos
+                  </Typography>
+                </CardContent>
+              </Box>
+            </Card>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* Segundo Carrossel */}
+      <Typography variant="h5" sx={{ mt: 3, mb: 2, pl: 2 }}>
+        Produtos
+      </Typography>
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{ clickable: true }}
+        loop={true}
+        spaceBetween={10}
+        slidesPerView={4}
+        breakpoints={{
+          320: { slidesPerView: 1 },
+          768: { slidesPerView: 2 },
+          1024: { slidesPerView: 4 },
+        }}
+      >
+        {products.slice(12, 18).map((product) => (
+          <SwiperSlide>
+            <Card
+              sx={{ minWidth: 300, position: "relative", margin: "0 auto" }}
+            >
+              <Box sx={{ position: "relative" }}>
+                <CardMedia
+                  component="img"
+                  height="100%"
+                  image={product.image}
+                  alt={product.title}
+                ></CardMedia>
+                <CardContent>
+                  <Typography variant="h6">{product.title}</Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {" "}
+                    {product.price} pontos
+                  </Typography>
+                </CardContent>
+              </Box>
+            </Card>
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
 };
