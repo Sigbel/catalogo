24ba220acd30821a9d6e1b6d243e88d2c8c9f7b2
Henrique B <hb.belgamo@gmail.com>
2024-12-08 19:37:51 -0300

    feat: adjusts mobile responsiveness

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,12 +10,18 @@ import {
   Typography,
   TextField,
   Box,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
 } from "@mui/material";
 
 // Icons
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import LogoutIcon from "@mui/icons-material/Logout";
 import HomeIcon from "@mui/icons-material/Home";
+import MenuIcon from "@mui/icons-material/Menu";
 import ProductCart from "../ProductCart/ProductCart";
 import SearchIcon from "@mui/icons-material/Search";
 
@@ -23,11 +29,11 @@ import SearchIcon from "@mui/icons-material/Search";
 import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useCart } from "../../contexts/CartContext";
-import { Search } from "@mui/icons-material";
+import { useMediaQuery } from "@mui/material";
 
 const NavBar = ({ setAuthenticated }) => {
   const navigate = useNavigate();
-  const location = useLocation()
+  const location = useLocation();
   const {
     cart,
     handleRemoveItem,
@@ -39,6 +45,8 @@ const NavBar = ({ setAuthenticated }) => {
   } = useCart();
   const user = JSON.parse(localStorage.getItem("user")) || "Usuário";
   const [isCartOpen, setCartOpen] = useState(false);
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const isMobile = useMediaQuery("(max-width: 600px)");
 
   const handleLogout = () => {
     localStorage.removeItem("user");
@@ -47,12 +55,12 @@ const NavBar = ({ setAuthenticated }) => {
   };
 
   useEffect(() => {
-    setSearchTerm("")
-  }, [location, setSearchTerm])
+    setSearchTerm("");
+  }, [location, setSearchTerm]);
 
   return (
     <>
-      <AppBar position="sticky" sx={{backgroundColor: "#5271ff"}}>
+      <AppBar position="sticky" sx={{ backgroundColor: "#5271ff" }}>
         <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
           <IconButton color="inherit" onClick={() => navigate("/home")}>
             <HomeIcon></HomeIcon>
@@ -82,42 +90,76 @@ const NavBar = ({ setAuthenticated }) => {
                 borderRadius: 1,
                 padding: "8px",
                 color: "#5271ff",
-                "&:hover": { backgroundColor: "rgba(255,255,255,0.8)" }
+                "&:hover": { backgroundColor: "rgba(255,255,255,0.8)" },
               }}
             >
               <SearchIcon></SearchIcon>
             </IconButton>
           </Box>
-          <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
-            <Box 
-              sx={{
-                backgroundColor: "#ffffff",
-                color: "#5271ff",
-                borderRadius: "10px",
-                padding: "4px 10px",
-                fontWeight: "bold",
-                display: "inline-flex",
-                alignItems: "center",
-                justifyContent: "center",
-              }}
-            >
-              {user.points} pontos
+
+          {!isMobile ? (
+            <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
+              <Box
+                sx={{
+                  backgroundColor: "#ffffff",
+                  color: "#5271ff",
+                  borderRadius: "10px",
+                  padding: "4px 10px",
+                  fontWeight: "bold",
+                  display: "inline-flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                {user.points} pontos
+              </Box>
+
+              <Typography
+                variant="body1"
+                sx={{ color: "#fff", fontWeight: "500" }}
+              >
+                Olá, {user.name}
+              </Typography>
+
+              <IconButton color="inherit" onClick={() => setCartOpen(true)}>
+                <Badge badgeContent={cart.length} color="error">
+                  <ShoppingCartIcon></ShoppingCartIcon>
+                </Badge>
+              </IconButton>
+
+              <IconButton color="inherit" onClick={handleLogout}>
+                <LogoutIcon></LogoutIcon>
+              </IconButton>
             </Box>
-
-            <Typography variant="body1" sx={{color: "#fff", fontWeight: "500"}}>Olá, {user.name}</Typography>
-
-            <IconButton color="inherit" onClick={() => setCartOpen(true)}>
-              <Badge badgeContent={cart.length} color="error">
-                <ShoppingCartIcon></ShoppingCartIcon>
-              </Badge>
+          ) : (
+            <IconButton color="inherit" onClick={() => setDrawerOpen(true)}>
+              <MenuIcon></MenuIcon>
             </IconButton>
-
-            <IconButton color="inherit" onClick={handleLogout}>
-              <LogoutIcon></LogoutIcon>
-            </IconButton>
-          </Box>
+          )}
         </Toolbar>
       </AppBar>
+
+      <Drawer
+        anchor="left"
+        open={drawerOpen}
+        onClose={() => setDrawerOpen(false)}
+      >
+        <List sx={{ width: 250 }} role="presentation">
+          <ListItem button onClick={() => navigate("/home")}>
+            <ListItemText primary="Home"></ListItemText>
+          </ListItem>
+          <Divider></Divider>
+          <ListItem button onClick={() => setCartOpen(true)}>
+            <ListItemText primary="Carrinho"></ListItemText>
+          </ListItem>
+          <Divider></Divider>
+          <ListItem button onClick={handleLogout}>
+            <ListItemText primary="Logout"></ListItemText>
+          </ListItem>
+          <Divider></Divider>
+        </List>
+      </Drawer>
+
       <ProductCart
         onClose={() => setCartOpen(false)}
         open={isCartOpen}
